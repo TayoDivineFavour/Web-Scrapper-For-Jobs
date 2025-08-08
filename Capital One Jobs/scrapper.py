6aaa9357345c6736ce62fa46f94377df9df1b124
= <tayodivinefavour@gmail.com>
2025-08-08 18:07:45 +0000

    we use beautiful soup to find the page number for pagination

--- Capital One Jobs/scrapper.py
+++ Capital One Jobs/scrapper.py
@@ -10,4 +10,9 @@ job_search = job.replace(' ' , '+')
 url = f'https://www.capitalonecareers.com/search-jobs?l=&k={job_search}&p=1'
 
 html = requests.get(url).text
-print(html)
+
+doc = BeautifulSoup(html , 'html.parser')
+
+pages_search = doc.find(class_="pagination-total-pages").text
+pages = int(str(pages_search).split(' ')[-1])
+print(pages)
